2eb196d07fa7c1b8732acf7b85099df7b3c22647
Gavriel Frant <gavril44@gmail.com>
2021-12-30 18:42:40 +0200

    refactor file stracture in back

--- back-end/index.ts
+++ back-end/index.ts
@@ -4,41 +4,15 @@ import { createServer } from "http";
 const server = createServer(app);
 import { Server, Socket } from "socket.io";
 import { ClientToServerEvents, ServerToClientEvents } from "./types";
-import { v4 as uuidv4 } from "uuid";
-import rooms from "./db/rooms";
+import chatHandler from "./socketHandlers/chatHandler";
 
 const io = new Server<ClientToServerEvents, ServerToClientEvents>(server);
 
-// const chatHandler = require("./socketHandlers/chatHandler");
+const onConnection = (socket: Socket) => {
+  chatHandler(socket);
+};
 
-io.on("connection", (socket) => {
-  let roomNum = "1";
-
-  socket.on("sendMessage", (message, cb) => {
-    switch (message.type) {
-      case "EnterRoomMessage":
-        roomNum = String(message.roomNum);
-        socket.join(roomNum);
-        socket.to(roomNum).emit("receiveMessage", message);
-        break;
-      case "message":
-        const newId = uuidv4();
-
-        const newMessage = {
-          ...message,
-          id: newId,
-        };
-
-        cb(newId);
-
-        socket.to(roomNum).emit("receiveMessage", newMessage);
-        break;
-      default:
-        break;
-    }
-    console.log(message);
-  });
-});
+io.on("connection", onConnection);
 
 server.listen(config.PORT, () => {
   `server listening on port ${config.PORT}`;
--- back-end/socketHandlers/chatHandler.ts
+++ back-end/socketHandlers/chatHandler.ts
@@ -1,5 +1,31 @@
-// export default function chatHandler(io: , socket) {
-//   socket.on("sendMessage", (message) => {
-//     socket.broadcast.emit("sendMessage", message);
-//   });
-// }
+import { Socket } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
+
+export default function chatHandler(socket: Socket) {
+  let roomNum = "1";
+
+  socket.on("sendMessage", (message, cb) => {
+    switch (message.type) {
+      case "EnterRoomMessage":
+        roomNum = String(message.roomNum);
+        socket.join(roomNum);
+        socket.to(roomNum).emit("receiveMessage", message);
+        break;
+      case "message":
+        const newId = uuidv4();
+
+        const newMessage = {
+          ...message,
+          id: newId,
+        };
+
+        cb(newId);
+
+        socket.to(roomNum).emit("receiveMessage", newMessage);
+        break;
+      default:
+        break;
+    }
+    console.log(message);
+  });
+}
